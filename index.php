241e7d96cf4d7baf2712b1232561546be8dc1af3
Gabriel Lima <lima20gabriel@gmail.com>
2024-08-05 14:53:29 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,22 @@
 <?php 
-    $dsn = 'mysql:host=localhost;dbname=xphp_com_pdo';
+    $dsn = 'mysql:host=localhost;dbname=php_com_pdo';
     $usuario = 'root';
     $senha = '';
 
     try {
         $conexao = new PDO($dsn, $usuario, $senha);
+
+        $query = '
+            select * from tb_usuario
+        ';
+
+        $stmt = $conexao->query($query);
+        $lista = $stmt->fetchAll(PDO::FETCH_OBJ);
+        echo '<pre>';
+        print_r($lista);
+        echo '</pre>';
+
+        echo $lista[2]->email;
     } catch (PDOException $e) {
         echo 'Erro: '.$e->getCode();
         echo '<hr>';
